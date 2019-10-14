0492c758c722a55a2d340260348830c932f4f998
Jorge Abreu <jorgeabreu87@gmail.com>
2019-10-14 20:38:43 -0300

    testing db

--- participar.php
+++ participar.php
@@ -80,7 +80,8 @@
 
 // LEVANTAR ID
 
-$pid = mysqli_query($cn,"Select id from easyquiz.participants where mail = 'j@j.com';");
+      $pid = mysqli_query($cn,"Select id from easyquiz.participants where mail = 'j@j.com';");
+      echo $pid;
       $ready = 1;
 //      echo $_POST['name'].'-'.$_POST['lastname'].'-'.$_POST['nick'].'-'.$_POST['email'].$_POST['datos'].$ready;
 
