8163ee848d1677281b7f9c3b89f0c782ffafec3a
Jorge Abreu <jorgeabreu87@gmail.com>
2019-10-15 22:09:41 -0300

    testing game logic

--- game.php
+++ game.php
@@ -10,80 +10,80 @@ if (mysqli_connect_errno()) {
 //TEST
 $tiempo = 0;
 $preguntastring= "";
+$tiempo60=0;
 
   if(isset($_GET['pid']))
   {
       //SANITIZAR GET PID PARA EVITAR INJECTION
-    $pid = $_GET['pid'];
-    $res = mysqli_query($cn,"Select timestamp from easyquiz.participants where id = '$pid';");
-    $sdata = mysqli_fetch_assoc($res);
-    if ($sdata['timestamp'] == "")
+    $_GET['pid'] = mysqli_escape_string($cn, $_PID['pid']);
+    if (ctype_digit($_GET['pid']))
     {
-      $tiempo=time();
-      $res = mysqli_query($cn,"UPDATE easyquiz.participants SET timestamp = '$tiempo' WHERE id = '$pid';");
-    }
-    
-  }
-
-  if(isset($_POST['datos']))
-  {
-    if ($_POST['datos'] != "0")
+      $pid = $_GET['pid'];
+      $res = mysqli_query($cn,"Select timestamp from easyquiz.participants where id = '$pid';");
+      if(mysqli_error($cn)!="")
+        {
+          echo mysqli_error($cn);
+          exit();
+        }
+      if (mysqli_num_rows($res) == 1)
       {
-         if (!ctype_digit($_POST['datos']))
-           {
-             //Levantar el tiempo del perfil en la DB
-             //$tiempo=
-             if ($tiempo == 0)
-               {
-                 //Obtener timestamp
-                 $tiempo=time();
-                 //Guardar timestamp en la DB
-                 // $tiempo
-                 //Obtener Timestamp+60
-                 $tiempo60=($tiempo+62);
-                 //Guardar tiempo +60 en el perfil de la DB
-                 // $tiempo60
-               }
-
-           $tiempo=time();
-           //Obtener T+60 de la DB
-           // $tiempo60
-           if ($tiempo < $tiempo60)
-             {
-               $preguntaid=rand(1, 15);
-               //levantar pregunta de la DB
-               //$preguntastring= "algo $preguntaid"
-
-               $respuestas=array(); 
-               array_push($respuestas, $preguntaid);
-
-               for ($i = 1; $i <= 3; $i++)
-               {
-                 $respid=rand(1, 15);
-                 if ($respid != $preguntaid)
-                 {
-                   array_push($respuestas, $respid);
-                 }
-                 else
-                 {
-                   $i--;
-                 }
-               }
-               sort($respuestas);
-
-             }
-         }
+        $sdata = mysqli_fetch_assoc($res);
+        if ($sdata['timestamp'] == "")
+        {
+          $tiempo=time();
+          $res = mysqli_query($cn,"UPDATE easyquiz.participants SET timestamp = '$tiempo' WHERE id = '$pid';");
+        }
+        else
+        {
+          $tiempo = $sdata['timestamp'];
+        }
       }
+      else
+      {
+        echo "ID is wrong. I dont know why, but is wrong"
+        exit();
+      }
+    } 
   }
 
+  if($pid)
+  {
+    if ($tiempo > 0)
+      {
+        //Get time +60
+        $tiempo60=($tiempo+9200);
+        //Guardar tiempo +60 en el perfil de la DB - Not sure is needed
+        
+        if ($tiempo < $tiempo60)
+        {
+          $preguntaid=rand(1, 15);
+          //levantar pregunta de la DB
+          //$preguntastring= "algo $preguntaid"
 
-//      echo $_POST['name'].'-'.$_POST['lastname'].'-'.$_POST['nick'].'-'.$_POST['email'].$_POST['datos'].$ready;
+          $respuestas=array(); 
+          array_push($respuestas, $preguntaid);
 
-//mysqli_close($enlace);
+          for ($i = 1; $i <= 3; $i++)
+          {
+            $respid=rand(1, 15);
+            if ($respid != $preguntaid)
+            {
+              array_push($respuestas, $respid);
+            }
+            else
+            {
+              $i--;
+            }
+          }
+          sort($respuestas);
+     
 
-//    }
+        }
+      
+      }
 
-//}
+  }
+
 ?>
 <html>
  <head>
